52db0d35ba7822394e566ff09fb56206b1135a33
Asmaa <asma.jazar.319@gmail.com>
2025-06-14 14:58:50 +0300

    Convert code from argv to prompt ==> (readline)

--- tasks/task3/main.c
+++ tasks/task3/main.c
@@ -1,30 +1,75 @@
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
+#include "../tasks.h"
 
 int main (int ac, char **av)
 {
-    int pid = fork ();
-    if (pid == 0)
+    (void)av;
+    if (ac != 1)
+        return 1;
+    while (1)
     {
-        int i = 1;
-        int j = 2;
-        char **args = (char **)malloc (sizeof (char *) * (ac - 1));
-        args[0] = "echo";
-        while (j < ac)
+        char *input = readline ("antshell$ ");
+        if (!input)
+            break ; 
+        add_history(input);
+        int pid = fork ();
+        if (pid == 0)
         {
-            args[i] = av[j];
-            i++;
-            j++;
+            int i = 0;
+            char d = '"';
+            char s = '\'';
+            int  d_qouted = 0;
+            int  s_qouted = 0;
+            char **args;
+
+            while (input[i])
+            {
+                if (strchr(input, d))
+                    d_qouted = 1;
+                else if (strchr (input, s))
+                    s_qouted = 1;
+                i++;
+            }
+            if (d_qouted)
+                args = ft_split (input, '"');
+            else if (s_qouted)
+                args = ft_split (input, '\'');
+            else
+                args = ft_split (input, ' ');
+            if (strcmp(args[0], "echo") == 0)
+            {
+                execve ("/usr/bin/echo", args, NULL);
+                perror ("execve");
+            }
+            else
+            {
+                execve (args[0], args, NULL);
+                perror (args[0]);
+            }
+            free (args);
+            exit (1);
         }
-        args[i] = NULL;
-        execve ("/usr/bin/echo", args, NULL);
-        perror ("execve");
-        free (args);
-        exit (1);
-    }
-    wait (NULL);
+        wait (NULL);
+    } 
     return 0;
 }
+
+// int pid = fork ();
+// if (pid == 0)
+// {
+//     int i = 1;
+//     int j = 2;
+//     char **args = (char **)malloc (sizeof (char *) * (ac - 1));
+//     args[0] = "echo";
+//     while (j < ac)
+//     {
+//         args[i] = av[j];
+//         i++;
+//         j++;
+//     }
+//     args[i] = NULL;
+//     args[i] = NULL;
+//     execve ("/usr/bin/echo", args, NULL);
+//     perror ("execve");
+//     free (args);
+//     exit (1);
+// }
